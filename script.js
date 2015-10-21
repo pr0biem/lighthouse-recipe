b47f333d9450a9bd176b00e6a5b52642114ce049
pr0biem <coleson.luterbach@gmail.com>
2015-10-20 23:04:47 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	console.log("Script included!");
+	$('.ing').click(function() {
+		$('.ing>span:hover').toggleClass('strike');
+	})
+	$('.step#unclicked').click(function() {
+		$('.step:hover').attr('id', $(this).attr('id') == "unclicked" ? "clicked" : "unclicked");
+	})
+	$('.hide').click(function() {
+		$('img').hide();
+	})
+	$('.show').click(function() {
+		$('img').show();
+	})
+});
